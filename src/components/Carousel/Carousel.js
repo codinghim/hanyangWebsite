e0b6ce2fbefde8ba76a8ccd3982a4c5e4c5c3a04
codinghim <codinghim0703@gmail.com>
2020-12-17 20:30:33 -0500

    carousel algorithm changed

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Carouselimg from '../Carouselimg/Carouselimg';
 import './carousel.css';
 
@@ -9,26 +9,53 @@ const Carousel = () => {
         <Carouselimg src={"https://s3-media0.fl.yelpcdn.com/bphoto/ohDnksLSoLxo2EkD-2IaQQ/o.jpg"} />,
         <Carouselimg src={"https://s3-media0.fl.yelpcdn.com/bphoto/8Ru2cssCnIBqNhFZvklMEQ/o.jpg"} />
     ]
-    const [x, setX] = useState(0)
+    const [slideIndex, setslideIndex] = useState(0)
+
     const goLeft = () =>{
-        console.log(x);
-        (x === 0) ? setX(-100*(arr.length-1)) : setX(x + 100);
+        if(slideIndex === 0){
+            setslideIndex(arr.length-1)
+        }else{
+            setslideIndex(slideIndex-1)
+        }
     }
     const goRight = () =>{
-        console.log(x);
-        (x === -100*(arr.length-1)) ? setX(0) : setX(x - 100)
+        if(slideIndex === (arr.length-1)){
+            setslideIndex(0)
+        }else{
+            setslideIndex(slideIndex+1)
+        }        
     }
+
+    const showSlide = (index) =>{
+        //make visible slide to invisible
+        const activeslides = document.getElementsByClassName('active-slide')
+        if(activeslides.length !== 0){
+            document.getElementsByClassName('active-slide')[0].className = "slide hidden-slide"
+        }
+        //show slide of an given index
+        document.getElementsByClassName('hidden-slide')[index].className = "slide active-slide"
+        console.log(`slide index: ${slideIndex}`)
+    }
+    
+    // When component mounted, set slide index as 0
+    useEffect(() => {
+        setslideIndex(0)
+    },[])
+
+    // Whenever slide Index is changed show slide Index
+    useEffect(() => {
+        showSlide(slideIndex)
+    },[slideIndex])
 
     return(
         <section className="carousel-container">
             {arr.map((item,index)=>{
                 return (
-                    <div key={index} className="carousel" style={{transform:`translateX(${x}%)`}}>
+                    <div key={index} className="slide hidden-slide">
                         {item}
                     </div>
                 )
             })}
-            {/* <img className="mainimg-image" src="https://s3-media0.fl.yelpcdn.com/bphoto/G0u3xzhYFb0LhhwLQZ7CuA/o.jpg" alt="restaurant view"/> */}
             <button id="goLeft" onClick={goLeft}>&lt;</button>
             <button id="goRight" onClick={goRight}>&gt;</button>
         </section>
